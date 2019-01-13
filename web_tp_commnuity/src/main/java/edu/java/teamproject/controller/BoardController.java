d21656198a33472df08b199998675fa47c79cf02
Taewon Eum <twon143@naver.com>
2019-01-14 01:33:12 +0900

    페이징 처리 기능 구현중

--- web_tp_commnuity/src/main/java/edu/java/teamproject/controller/BoardController.java
+++ web_tp_commnuity/src/main/java/edu/java/teamproject/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.java.teamproject.service.BoardService;
 import edu.java.teamproject.util.Criteria;
+import edu.java.teamproject.util.PageMaker;
 
 @Controller
 @RequestMapping(value = "board")
@@ -31,10 +32,16 @@ public class BoardController {
       logger.info("글쓰기 테스트 호출 ");
    }
    
-   @RequestMapping(value = "listCriteria", method = RequestMethod.GET)
+   @RequestMapping(value = "listPaging", method = RequestMethod.GET)
    public void listCriteria(Model model, Criteria criteria) {
-	   logger.info("listCriteria({})", criteria);
+	   logger.info("listPaging({})", criteria);
+	   
+	   PageMaker pageMaker = new PageMaker();
+	   pageMaker.setCriteria(criteria);
+	   pageMaker.setTotalCount(1000);
+	   
 	   model.addAttribute("boardList", boardService.listCriteria(criteria));
+	   model.addAttribute("pageMaker", pageMaker);
 	   
    }
 }
